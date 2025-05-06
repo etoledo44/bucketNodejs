3687a5a83d60c1bd55980efd4bf86a81eef53529
Érique Toledo <eriquetoledo@outlook.com>
2025-05-06 10:42:33 -0300

    adiciona arquivo principal da aplicação em TypeScript, configurando o servidor Express, middleware e rotas, além de carregar variáveis de ambiente

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,18 @@
-require("dotenv").config(); // Carrega variáveis de ambiente do arquivo .env
+import dotenv from 'dotenv';
+dotenv.config(); // Carrega variáveis de ambiente do arquivo .env
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const CONSTANTS = require("./config/constants");
+import express from 'express';
+import bodyParser from 'body-parser';
+import CONSTANTS from './config/constants';
 
-const { expressjwt } = require("express-jwt");
-const { generateToken } = require("./jwt");
-const routes = require("./routes");
+import { expressjwt } from 'express-jwt';
+import { generateToken } from './jwt/index';
+import routes from './routes/index';
 
 const app = express();
 
 // Configura o middleware para servir arquivos estáticos
-app.use("/visualizar", express.static(CONSTANTS.uploadDir));
+app.use('/visualizar', express.static(CONSTANTS.uploadDir));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
